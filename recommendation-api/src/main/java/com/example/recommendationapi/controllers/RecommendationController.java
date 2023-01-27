68a945663d0e2c5951fd8f0d14c72ed57ab3fd08
andrasimion99 <61377543+andrasimion99@users.noreply.github.com>
2023-01-27 18:04:18 +0200

    added cross origin

--- recommendation-api/src/main/java/com/example/recommendationapi/controllers/RecommendationController.java
+++ recommendation-api/src/main/java/com/example/recommendationapi/controllers/RecommendationController.java
@@ -6,16 +6,14 @@ import com.example.recommendationapi.models.TopVinylByCount;
 import com.example.recommendationapi.models.UserPreferences;
 import com.example.recommendationapi.services.SparqlService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.inject.Inject;
 
 @RestController
 @RequestMapping("/recommendation")
+@CrossOrigin
 public class RecommendationController {
 
     private final SparqlService sparqlService;
